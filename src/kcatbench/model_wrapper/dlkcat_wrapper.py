515d4d07487825eca4994b87a3b6f4ed3ab6c60f
Johann <johann.lottermoser@outlook.de>
2025-11-21 16:51:36 +0100

    feat(wrapper): basic predict structure

--- src/kcatbench/model_wrapper/dlkcat_wrapper.py
+++ src/kcatbench/model_wrapper/dlkcat_wrapper.py
@@ -11,8 +11,10 @@ if str(DLKCAT_CODE_DIR) not in sys.path:
 
 from .base import BaseModel
 # from prediction_for_input import split_sequence
+import model
 import pandas as pd
 import zipfile
+import torch
 
 
 class DLKcatWrapper(BaseModel):
@@ -32,7 +34,51 @@ class DLKcatWrapper(BaseModel):
             zip_ref.extractall(DLKCAT_DATA_DIR)
 
     def predict(self, input_data: pd.DataFrame) -> pd.DataFrame:
-        # ignore for now
+        # Based on the predicition_for_input script from DLKcat
+
+        fingerprint_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "fingerprint_dict.pickle")
+        atom_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "atom_dict.pickle")
+        bond_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "bond_dict.pickle")
+        word_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "sequence_dict.pickle")
+        
+        n_fingerprint = len(fingerprint_dict)
+        n_word = len(word_dict)
+
+        radius=2
+        ngram=3
+
+        dim=10
+        layer_gnn=3
+        side=5
+        window=11
+        layer_cnn=3
+        layer_output=3
+        lr=1e-3
+        lr_decay=0.5
+        decay_interval=10
+        weight_decay=1e-6
+        iteration=100
+
+        if torch.cuda.is_available():
+            device = torch.device('cuda')
+        else:
+            device = torch.device('cpu')
+
+        # torch.manual_seed(1234)
+        Kcat_model = model.KcatPrediction(device, n_fingerprint, n_word, 2*dim, layer_gnn, window, layer_cnn, layer_output).to(device)
+        Kcat_model.load_state_dict(torch.load('../../Results/output/all--radius2--ngram3--dim20--layer_gnn3--window11--layer_cnn3--layer_output3--lr1e-3--lr_decay0.5--decay_interval10--weight_decay1e-6--iteration50', map_location=device))
+        # print(state_dict.keys())
+        # model.eval()
+        predictor = Predictor(Kcat_model)
+
         return pd.DataFrame()
 
+class Predictor(object):
+    def __init__(self, model):
+        self.model = model
+
+    def predict(self, data):
+        predicted_value = self.model.forward(data)
+
+        return predicted_value
 
