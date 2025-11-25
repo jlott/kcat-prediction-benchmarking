dfdb69b364e49a3ff3b743b445eeab396b71b8ee
Johann <johann.lottermoser@outlook.de>
2025-11-25 17:54:59 +0100

    feat(wrapper): predict structure for DLKcat done

--- src/kcatbench/model_wrapper/dlkcat_wrapper.py
+++ src/kcatbench/model_wrapper/dlkcat_wrapper.py
@@ -10,15 +10,25 @@ if str(DLKCAT_CODE_DIR) not in sys.path:
 
 
 from .base import BaseModel
-# from prediction_for_input import split_sequence
 import model
 import pandas as pd
+import numpy as np
 import zipfile
 import torch
+import requests
+import math
+from rdkit import Chem
+from collections import defaultdict
 
 
 class DLKcatWrapper(BaseModel):
     name = "DLKcat"
+
+    fingerprint_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "fingerprint_dict.pickle")
+    atom_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "atom_dict.pickle")
+    bond_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "bond_dict.pickle")
+    word_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "sequence_dict.pickle")
+    
 
     def __init__(self):
         super().__init__()
@@ -36,13 +46,8 @@ class DLKcatWrapper(BaseModel):
     def predict(self, input_data: pd.DataFrame) -> pd.DataFrame:
         # Based on the predicition_for_input script from DLKcat
 
-        fingerprint_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "fingerprint_dict.pickle")
-        atom_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "atom_dict.pickle")
-        bond_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "bond_dict.pickle")
-        word_dict = model.load_pickle(DLKCAT_DATA_DIR / "input" / "sequence_dict.pickle")
-        
-        n_fingerprint = len(fingerprint_dict)
-        n_word = len(word_dict)
+        n_fingerprint = len(self.fingerprint_dict)
+        n_word = len(self.word_dict)
 
         radius=2
         ngram=3
@@ -64,14 +69,164 @@ class DLKcatWrapper(BaseModel):
         else:
             device = torch.device('cpu')
 
-        # torch.manual_seed(1234)
         Kcat_model = model.KcatPrediction(device, n_fingerprint, n_word, 2*dim, layer_gnn, window, layer_cnn, layer_output).to(device)
         Kcat_model.load_state_dict(torch.load('../../Results/output/all--radius2--ngram3--dim20--layer_gnn3--window11--layer_cnn3--layer_output3--lr1e-3--lr_decay0.5--decay_interval10--weight_decay1e-6--iteration50', map_location=device))
-        # print(state_dict.keys())
-        # model.eval()
         predictor = Predictor(Kcat_model)
 
-        return pd.DataFrame()
+        required_cols = {"name", "smiles", "sequence"}
+        missing = required_cols - set(input_data.columns)
+
+        if missing:
+            raise ValueError(f"Missing required columns in input data: {missing}")
+
+        results = []
+
+        for row in input_data.itertuples(index=True):
+            name = row.name
+            smiles = row.smiles
+            sequence = row.sequence
+
+            result = {
+                "name": name,
+                "smiles": smiles,
+                "sequence": sequence,
+                "kcat_prediction": pd.NA,
+            }
+
+            if pd.isna(smiles):
+                if pd.isna(name):
+                    results.append(result)
+                    continue
+                else:
+                    smiles = self.get_smiles(name)
+                    
+            if pd.isna(sequence):
+                results.append(result)
+                continue
+
+            try:
+                mol = Chem.AddHs(Chem.MolFromSmiles(smiles))
+
+                atoms = self.create_atoms(mol)
+
+                i_jbond_dict = self.create_ijbonddict(mol)
+
+                fingerprints = self.extract_fingerprints(atoms, i_jbond_dict, radius)
+                
+                adjacency = np.array(Chem.GetAdjacencyMatrix(mol))
+
+                words = self.split_sequence(sequence, ngram)
+
+                fingerprints = torch.LongTensor(fingerprints).to(device)
+                adjacency = torch.FloatTensor(adjacency).to(device)
+                words = torch.LongTensor(words).to(device)
+
+                inputs = [fingerprints, adjacency, words]
+
+                prediction = predictor.predict(inputs)
+                kcat_log_value = prediction.item()
+                kcat_value = '%.4f' %math.pow(2, kcat_log_value)
+
+                result["kcat_prediction"] = kcat_value
+                results.append(result)
+            except:
+                results.append(result)
+                continue
+
+        return pd.DataFrame(results)
+    
+    def get_smiles(name):
+        try :
+            url = 'https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/name/%s/property/CanonicalSMILES/TXT' % name
+            req = requests.get(url)
+            if req.status_code != 200:
+                smiles = pd.NA
+            else:
+                smiles = req.content.splitlines()[0].decode()
+        except :
+            smiles = pd.NA
+
+        return smiles
+
+    def create_atoms(self, mol):
+        """Create a list of atom (e.g., hydrogen and oxygen) IDs
+        considering the aromaticity."""
+        atoms = [a.GetSymbol() for a in mol.GetAtoms()]
+        for a in mol.GetAromaticAtoms():
+            i = a.GetIdx()
+            atoms[i] = (atoms[i], 'aromatic')
+        atoms = [self.atom_dict[a] for a in atoms]
+
+        return np.array(atoms)
+    
+    def create_ijbonddict(self, mol):
+        """Create a dictionary, which each key is a node ID
+        and each value is the tuples of its neighboring node
+        and bond (e.g., single and double) IDs."""
+        i_jbond_dict = defaultdict(lambda: [])
+        for b in mol.GetBonds():
+            i, j = b.GetBeginAtomIdx(), b.GetEndAtomIdx()
+            bond = self.bond_dict[str(b.GetBondType())]
+            i_jbond_dict[i].append((j, bond))
+            i_jbond_dict[j].append((i, bond))
+        return i_jbond_dict
+    
+    def extract_fingerprints(self, atoms, i_jbond_dict, radius):
+        """Extract the r-radius subgraphs (i.e., fingerprints)
+        from a molecular graph using Weisfeiler-Lehman algorithm."""
+
+        if (len(atoms) == 1) or (radius == 0):
+            fingerprints = [self.fingerprint_dict[a] for a in atoms]
+
+        else:
+            nodes = atoms
+            i_jedge_dict = i_jbond_dict
+
+            for _ in range(radius):
+
+                """Update each node ID considering its neighboring nodes and edges
+                (i.e., r-radius subgraphs or fingerprints)."""
+                fingerprints = []
+                for i, j_edge in i_jedge_dict.items():
+                    neighbors = [(nodes[j], edge) for j, edge in j_edge]
+                    fingerprint = (nodes[i], tuple(sorted(neighbors)))
+                    try :
+                        fingerprints.append(self.fingerprint_dict[fingerprint])
+                    except :
+                        self.fingerprint_dict[fingerprint] = 0
+                        fingerprints.append(self.fingerprint_dict[fingerprint])
+
+                nodes = fingerprints
+
+                """Also update each edge ID considering two nodes
+                on its both sides."""
+                _i_jedge_dict = defaultdict(lambda: [])
+                for i, j_edge in i_jedge_dict.items():
+                    for j, edge in j_edge:
+                        both_side = tuple(sorted((nodes[i], nodes[j])))
+                        try :
+                            edge = self.edge_dict[(both_side, edge)]
+                        except :
+                            self.edge_dict[(both_side, edge)] = 0
+                            edge = self.edge_dict[(both_side, edge)]
+
+                        _i_jedge_dict[i].append((j, edge))
+                i_jedge_dict = _i_jedge_dict
+
+        return np.array(fingerprints)
+    
+    def split_sequence(self, sequence, ngram):
+        sequence = '-' + sequence + '='
+        words = list()
+        for i in range(len(sequence)-ngram+1) :
+            try :
+                words.append(self.word_dict[sequence[i:i+ngram]])
+            except :
+                self.word_dict[sequence[i:i+ngram]] = 0
+                words.append(self.word_dict[sequence[i:i+ngram]])
+
+        return np.array(words)
+    
 
 class Predictor(object):
     def __init__(self, model):
@@ -81,4 +236,5 @@ class Predictor(object):
         predicted_value = self.model.forward(data)
 
         return predicted_value
+
 
